553c895dd4d7820053fc3315180aa715332287ca
Edward Smith <esmith78@gmail.com>
2023-05-30 17:09:24 -0400

    lab to study for code challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -11,5 +12,42 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       toyFormContainer.style.display = "none";
     }
-  });
-});
+  })
+
+  const URL ="http://localhost:3000/toys"
+  fetch(URL)
+  // this converts json to a javascript object
+  .then(response=>response.json())
+  .then(toys=>{
+    toys.forEach(toy=>createToy(toy))
+  })
+})
+// function to create the toy card
+function createToy(toy) {
+// this grabs the div id toy collection
+  const toyList=document.querySelector('#toy-collection')
+  // get toy name
+  const name = document.createElement('h2')
+  name.textContent=toy.name
+  // get toy image
+  const image = document.createElement('img')
+  image.src=toy.image
+  image.classList.add('toy-avatar')
+  // p tag with likes look in db.json for key name which is likes
+  const likes =document.createElement('p')
+  likes.textContent=`${toy.likes} like`
+  // or alternatively likes.textContent=toy.likes+' likes'
+  likes.classList.add('like-btn')
+  // add a like button
+  const button = document.createElement('button')
+  button.textContent='like'
+  button.id=toy.id
+  button.classList.add('like-btn')
+  // add toy card 'div' all above elements will be added
+  // always do the appended element last
+  const card=document.createElement('div')
+  card.append(name, image, likes, button)
+  card.classList.add('card')
+  // add toy card to toyList or collection
+  toyList.append(card)
+}
